b9f5545335091fae9f80dbbcee15e71054bcec3c
Rahul Pandey <rahulpandeyr184@gmail.com>
2025-03-02 22:57:42 +0530

    Like Feature Code Uploded

---
+++ like_button_feature/script.js
@@ -0,0 +1,17 @@
+let imageContainer = document.querySelector('.image');
+let love = document.querySelector('i');
+
+imageContainer.addEventListener('dblclick',function(){
+    love.style.transform = 'translate(-50%,-50%) scale(1)'
+
+    love.style.opacity = '0.8'
+
+    setTimeout(function(){
+        love.style.opacity = '0'
+    },2000);
+    
+    setTimeout(function(){
+        love.style.transform = 'translate(-50%,-50%) scale(0)'
+    },2000);
+})
+
